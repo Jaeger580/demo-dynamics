bba335b4e7b2226882cb5c2a55d1038ca0e274dd
Jaeger580 <91155249+Jaeger580@users.noreply.github.com>
2022-04-20 22:50:02 -0400

    Movement Controls added
    Project has actual begun with this update

--- Demo Dynamics/Assets/Scripts/Player/PlayerController.cs
+++ Demo Dynamics/Assets/Scripts/Player/PlayerController.cs
@@ -1,26 +1,81 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
+// This script is for handling the controls and animations of the player character
 
 public class PlayerController : MonoBehaviour
 {
-
     private CharacterController controller;
-    private Vector3 playerVelocity;
-    private bool groundedPlayer;
-    private float playerSpeed = 2.0f;
-    private float jumpHeight = 1.0f;
-    //private float gravityValue = -9.81f;
+    private PlayerInput playerInput;
+
+    private Vector2 currentMovementInput;
+    private Vector3 currentMovement;
+    private bool isMovementPressed;
+
+    [SerializeField]
+    float walkSpeed;
+
+    [SerializeField]
+    float sprintSpeed;
+
+    void Awake()
+    {
+        playerInput = new PlayerInput();
+        controller = gameObject.GetComponent<CharacterController>();
+
+        // Update movement whenever a movement button is pressed or released
+        playerInput.PlayerControls.Move.started += onMovementInput;
+        playerInput.PlayerControls.Move.canceled += onMovementInput;
+        // Update movement for gamepads with analoge sticks
+        playerInput.PlayerControls.Move.performed += onMovementInput;
+
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        controller = gameObject.GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Moves the player using the Character controller component
+        handleGravity();
+        controller.Move(currentMovement * walkSpeed * Time.deltaTime);
+    }
+
+    void onMovementInput(InputAction.CallbackContext context) 
+    {
+        currentMovementInput = context.ReadValue<Vector2>();
+        currentMovement.x = currentMovementInput.x;
+        currentMovement.z = currentMovementInput.y;
+        isMovementPressed = currentMovementInput.x != 0 || currentMovementInput.y != 0;
+    }
+
+    // Method that adds gravity to the player since there is no rigidbody
+    void handleGravity() 
+    {
+        if (controller.isGrounded)
+        {
+            float groundGravity = -0.01f;
+            currentMovement.y = groundGravity;
+        }
+        else 
+        {
+            float gravity = -9.81f;
+            currentMovement.y = gravity;
+        }
+    }
+
+    void OnEnable()
+    {
+        playerInput.PlayerControls.Enable();
+    }
+
+    void OnDisable()
+    {
+        playerInput.PlayerControls.Disable();
     }
 }
